9bb48f005c3d81de212f08c6034d3c75e4b1a527
James Booth <jabooth@gmail.com>
2014-01-08 18:34:14 +0000

    some changes away from paddys data

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,11 +7,11 @@ import os
 base_path = os.path.dirname(os.path.realpath(__file__))
 THEME = '{}/themes/pelican-responsive/responsive'.format(base_path)
 
-AUTHOR = u'Patrick Snape'
-SITENAME = u'patricksnape.github.io'
-SITEURL = 'http://patricksnape.github.io'
+AUTHOR = u'James Booth'
+SITENAME = u'jabooth.github.io'
+SITEURL = 'http://jabooth.github.io'
 MINI_BIO = u"PhD Candidate at Imperial College London"
-BIO = u'<strong>Patrick Snape</strong> has some sort of BIO text'
+BIO = u'<strong>James Booth</strong> has some sort of BIO text'
 
 TIMEZONE = 'Europe/London'
 
@@ -27,9 +27,9 @@ LINKS = ()
 
 # Social widget
 SOCIAL = (
-    ('Github', 'https://github.com/patricksnape', '&#xe037;'),
-    ('Google Plus', 'https://plus.google.com/+PatrickSnape', '&#xe039;'),
-    ('Twitter', 'https://twitter.com/berecursive', '&#xe086;'),
+    ('Github', 'https://github.com/jabooth', '&#xe037;'),
+    ('Google Plus', 'https://plus.google.com/+JamesBoothUK', '&#xe039;'),
+    ('Twitter', 'https://twitter.com/jamesabooth', '&#xe086;'),
 )
 
 DEFAULT_PAGINATION = 10
@@ -43,4 +43,6 @@ PLUGINS = ['liquid_tags.notebook', 'liquid_tags.include_code']
 LIQUID_TAGS_IPYTHON_STYLES = open('_nb_header.html').read().decode('utf-8')
 
 PATH = '{}/content'.format(base_path)
+# all these are copied into the output folder
 STATIC_PATHS = ['images', 'code', 'notebooks', 'theme/img/avatar.jpg']
+
